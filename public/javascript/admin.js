d8b23b49fa015cba846308b27cef0ae6e4426744
scalaview <chailink100@gmail.com>
2016-05-07 14:29:03 +0800

    fix kindeditor-all-min.js

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,6 +1,6 @@
 //=  require jquery/dist/jquery.min.js
 //=  require bootstrap/dist/js/bootstrap.min.js
-//=  require kindeditor/kindeditor-min.js
+//=  require kindeditor/kindeditor-all-min.js
 //=  require bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js
 //=  require handlebars/handlebars.min.js
 //=  require select2/dist/js/select2.min.js
