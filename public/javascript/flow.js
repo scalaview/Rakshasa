a76b411aedb0676ee7b688c90ad5e2f6c79e2803
sacalview <chailink100@gmail.com>
2017-04-24 17:16:44 +0800

    remove block

--- public/javascript/flow.js
+++ public/javascript/flow.js
@@ -1,6 +1,7 @@
-//=  require  zepto/zepto.min.js
+//=  require  jquery/dist/jquery.min.js
+//=  require  bootstrap/dist/js/bootstrap.min.js
 //=  require  js/publicdata.js
 //=  require  handlebars/handlebars.min.js
+//=  require  js/base.js
+//=  require  js/rechargeflow.js
 //=  require  js/layer.js
-//=  require  js/sm.min.js
-//=  require  js/swipeslide.min.js
