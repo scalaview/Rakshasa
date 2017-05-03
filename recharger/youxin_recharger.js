4792681fea746729222c80d1ed7bac96932173c9
scalaview <chailink100@gmail.com>
2017-05-04 00:30:23 +0800

    add sync products

--- recharger/youxin_recharger.js
+++ recharger/youxin_recharger.js
@@ -9,6 +9,87 @@ function YouXingRecharge(appid, key, host){
   this.host = host
 }
 
+YouXingRecharge.prototype.syncProducts = function(products, type){
+  var models  = require('../models')
+  var async = require("async")
+
+  function getValue(string){
+    var y = /[M|G]/,
+        end = y.exec(string)
+    if(!end) return 0
+    if(end.index + 1 <= string.length){
+      var unit = string.substring(end.index, end.index + 1)
+    }else{
+      var unit = 'M'
+    }
+    var size = string.replace(/[^0-9]/ig,"")
+    if(unit.toLowerCase() == 'g' ){
+      return parseInt(size) * 1024
+    }else{
+      return parseInt(size)
+    }
+  }
+
+  async.each(products, function(product, pass){
+    async.waterfall([function(next){
+      models.TrafficPlan.findOrCreate({
+        where: {
+          bid: product.pid,
+          type: models.TrafficPlan.TYPE[type]
+        },
+        defaults: {
+          providerId: models.TrafficPlan.Provider[product.provider],
+          value: getValue(product.name),
+          name: product.name,
+          cost: parseFloat(product.purchase_price) + 3.00,
+          display: false,
+          type: models.TrafficPlan.TYPE[type],
+          bid: product.pid,
+          purchasePrice: product.purchase_price
+        }
+      }).spread(function(trafficPlan){
+        next(null, trafficPlan)
+      }).catch(function(err){
+        next(err)
+      })
+    }, function(trafficPlan, next){
+      models.TrafficGroup.findOrCreate({
+        where: {
+          name: trafficPlan.name,
+          providerId: trafficPlan.providerId
+        },
+        defaults: {
+          name: trafficPlan.name,
+          providerId: trafficPlan.providerId,
+          display: false
+        }
+      }).spread(function(trafficGroup){
+        next(null, trafficPlan, trafficGroup)
+      }).catch(function(err){
+        next(err)
+      })
+    }, function(trafficPlan, trafficGroup, next){
+      trafficPlan.updateAttributes({
+        trafficGroupId: trafficGroup.id
+      }).then(function(trafficPlan){
+        next(null, trafficPlan, trafficGroup)
+      }).catch(function(err){
+        next(err)
+      })
+    }], function(err, trafficPlan, trafficGroup){
+      if(err){
+        pass(err)
+      }else{
+        pass(null, trafficPlan, trafficGroup)
+      }
+    })
+  }, function(err){
+    if(err){
+      console.log(err)
+    }
+  })
+}
+
 YouXingRecharge.prototype.sign = function(params){
   var strArray = [],
       keys = Object.keys(params).sort()
