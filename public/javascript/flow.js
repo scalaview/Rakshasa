878f2c47a3997d1e6004a875ad7bf844d4b77f92
scalaview <chailink100@gmail.com>
2017-05-13 22:50:50 +0800

    add banner

--- public/javascript/flow.js
+++ public/javascript/flow.js
@@ -1,5 +1,6 @@
 //=  require  jquery/dist/jquery.min.js
 //=  require  bootstrap/dist/js/bootstrap.min.js
+//=  require  bxslider-4/dist/jquery.bxslider.min.js
 //=  require  js/publicdata.js
 //=  require  handlebars/handlebars.min.js
 //=  require  js/base.js
